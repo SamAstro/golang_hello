d2307b1e755e7e59f76dbdb0332a05772d5e6c61
Drappeau, Samia <samia.drappeau@continental-corporation.com>
2017-08-07 23:24:31 +0200

    Creating my first package and using it.

--- golang_hello.go
+++ golang_hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/samastro/stringutil"
+)
 
 func main(){
     fmt.Print("hello, goland world\n")
+    fmt.Printf(stringutil.Reverse("\n!oG ,olleH"))
     }
